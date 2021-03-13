68d743af92005750469bb4a9d7ba7c3e5385c5aa
yagomouro <yagormouro@gmail.com>
2021-03-13 00:22:46 -0300

    cria devflix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+// Configurações do Owl Carousel
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 2
         },
-        1000:{
-            items:5
+        1000: {
+            items: 3
         }
     }
 })
+
+// Header scroll
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 50){
+        $(".header").addClass("scroll");
+    }
+    else{
+        $(".header").removeClass("scroll");
+    }
+});
+
